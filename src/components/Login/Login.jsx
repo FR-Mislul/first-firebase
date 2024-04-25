fdc924d79078625af0db3d56300a4f96f9f00556
FR Mislul <frmislul421@gmail.com>
2024-04-26 00:46:58 +0600

    google propup login with firebase app creat

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,17 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../fairbase/fairbase.init";
 import { useState } from "react";
+// import { GithubAuthProvider } from "firebase/auth";
 // import { GoogleAuthProvider } from "firebase/auth";
 
 const Login = () => {
     const [user, setUser] = useState(null)
     const auth = getAuth(app);
-    const provider = new GoogleAuthProvider();
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider =new GithubAuthProvider()
     const handelGoogleSingIn = () => {
         // console.log('mama is coming')
-        signInWithPopup(auth, provider)
+        signInWithPopup(auth, googleProvider)
             .then(result => {
                 const loggedInUser = result.user;
                 setUser(loggedInUser)
@@ -18,6 +20,19 @@ const Login = () => {
             .catch(error => {
                 console.log('error', error.message)
             })
+    }
+
+    const handelGithubSingIn = () => {
+        // console.log('ami github sing in')
+        signInWithPopup(auth, githubProvider)
+        .then(result => {
+            const loggedInUser = result.user;
+                setUser(loggedInUser)
+                console.log(loggedInUser)
+        })
+        .catch(error => {
+            console.log(error)
+        })
     }
 
     const handelSingOut = () => {
@@ -36,7 +51,10 @@ const Login = () => {
         <div>
             {user ?
                 <button onClick={handelSingOut}>Sing out</button> :
-                <button onClick={handelGoogleSingIn}>Google Login</button>}
+                <div>
+                    <button onClick={handelGoogleSingIn}>Google Login</button>
+                    <button onClick={handelGithubSingIn}>Github SingIn</button>
+                </div>}
             {user && <div>
                 <h2>User: {user?.displayName}</h2>
                 <h3>Email: {user?.email}</h3>
